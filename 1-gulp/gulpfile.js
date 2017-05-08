c064ef655f49b96a105bab15fa02078a413250f9
Arvind <europa1613@users.noreply.github.com>
2017-05-07 23:20:49 -0400

    gulp-livereload

--- 1-gulp/gulpfile.js
+++ 1-gulp/gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var livereload = require('gulp-livereload');
 
 var SCRIPTS_PATH = 'public/scripts/**/*.js';
 
@@ -14,7 +15,8 @@ gulp.task('scripts', function() {
 
     return gulp.src(SCRIPTS_PATH)
         .pipe(uglify())
-        .pipe(gulp.dest('public/dist'));
+        .pipe(gulp.dest('public/dist'))
+        .pipe(livereload());
 });
 
 //Images
@@ -31,5 +33,6 @@ gulp.task('default', function() {
 gulp.task('watch', function() {
 	console.log('Starting watch task');
 	require('./server.js');
+	livereload.listen();
 	gulp.watch(SCRIPTS_PATH, ['scripts']);
 });
