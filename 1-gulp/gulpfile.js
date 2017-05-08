a668e75d2f29017a95ee5b21e86d7e2e2b6cf20f
Arvind <europa1613@users.noreply.github.com>
2017-05-07 21:16:45 -0400

    basic shell tasks

---
+++ 1-gulp/gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+
+//Styles
+gulp.task('styles', function() {
+	console.log('Starting styles task');
+});
+
+//Scripts
+gulp.task('scripts', function() {
+	console.log('Starting scripts task');
+});
+
+//Images
+gulp.task('images', function() {
+	console.log('Starting images task');
+});
+
+//Default
+gulp.task('default', function() {
+	console.log('Starting default task');
+});
