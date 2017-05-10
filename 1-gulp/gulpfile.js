ff1fdec11c690cbee1e8828fdadc7314ceda0750
Arvind <europa1613@users.noreply.github.com>
2017-05-09 21:47:14 -0400

    Error Handling - gulp-plumber

--- 1-gulp/gulpfile.js
+++ 1-gulp/gulpfile.js
@@ -4,6 +4,7 @@ var livereload = require('gulp-livereload');
 var concat = require('gulp-concat');
 var minifyCss = require('gulp-minify-css');
 var autoprefixer = require('gulp-autoprefixer');
+var plumber = require('gulp-plumber');
 
 var SCRIPTS_PATH = 'public/scripts/**/*.js';
 var CSS_PATH = 'public/css/**/*.css';
@@ -13,9 +14,14 @@ gulp.task('styles', function() {
     console.log('Starting styles task');
 
     return gulp.src(['public/css/reset.css', CSS_PATH])
-    	//gulp.src(CSS_PATH) //this add the reset at the end in concatinated file, making the reset to override.
+        //gulp.src(CSS_PATH) //this add the reset at the end in concatinated file, making the reset to override.
+        .pipe(plumber(function(err) {
+            console.log('Styles task Error!');
+            console.log(err);
+            this.emit('end');
+        }))
         .pipe(autoprefixer({
-        	browsers: ['last 2 versions', 'ie 8']
+            browsers: ['last 2 versions', 'ie 8']
         }))
         .pipe(concat('styles.css'))
         .pipe(minifyCss())
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # gulp
 Everything Gulp
+
+## Running
+`yarn install`
+`gulp <task>`
+
+### Available gulp task
+- styles
+- scripts
+- images
+- default
+- watch (will the livereload)
+
+
+### Application
+`localhost:3000`
